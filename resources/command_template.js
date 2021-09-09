e0ee3b07d367c4362c14010abed200c8a3d9634c
Patrick Unick <punick2022@CCHSED.LOCAL>
2021-09-09 12:10:33 -0700

    [Pat] make command template object

---
+++ resources/command_template.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    name        : "name",
+    description : "description",
+    cooldown    : 1,
+    aliases     : ["alias"],
+    guildOnly   : true,
+    privileges  : ["SEND_MESSAGES"],
+    usage       : "cmd",
+    
+    async execute(interaction) {
+        // runs on execution
+    }
+};
